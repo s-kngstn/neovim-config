33502fea852aa837e6b1409c19b274a34636f806
s-kngstn <s.kngstn@protonmail.com>
2023-03-09 23:01:53 +0000

    tester

--- lua/skngstn/remap.lua
+++ lua/skngstn/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- return to file tree
 
--- split window vertically
+-- split window vertically a
 vim.keymap.set("n", "<C-s>", "<C-w>v")
 
 -- window movement
