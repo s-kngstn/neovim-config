17eff96f629e9b4906e2a930c99375719c7fd864
s-kngstn <s.kngstn@protonmail.com>
2023-03-09 20:05:25 +0000

    add more remaps

--- lua/skngstn/remap.lua
+++ lua/skngstn/remap.lua
@@ -1,10 +1,25 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- return to file tree
 
+-- window movement
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+-- terminal movement
+vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<CR>")
+vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<CR>")
+vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<CR>")
+vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<CR>")
 
 -- highlight and move text
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- highlight and move text horizontal
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 -- stay centered when hopping through code
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
